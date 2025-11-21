4f50477d0e66e758d6d5ab58302be65dd6f4b6b8
ALPHA-DEV-BLOCKCHAIN <ibrahimmainainc@gmail.com>
2025-11-21 10:25:41 +0300

    writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,26 +1,71 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+// Interface for the constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-interface Director extends Teacher {
-  numberOfReports: number;
+// Interface for the class
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
 }
 
-interface printTeacherFunction {
-  firstName: string;
-  lastName: string;
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
 
-function printTeacher({ firstName, lastName }: printTeacherFunction): string {
-  return `${firstName}. ${lastName}`;
-}
+export { StudentClass };
 
-console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// interface Teacher {
+//   readonly firstName: string;
+//   readonly lastName: string;
+//   fullTimeEmployee: boolean;
+//   yearsOfExperience?: number;
+//   location: string;
+//   [key: string]: any;
+// }
+
+// interface Director extends Teacher {
+//   numberOfReports: number;
+// }
+
+// interface printTeacherFunction {
+//   firstName: string;
+//   lastName: string;
+// }
+
+// function printTeacher({ firstName, lastName }: printTeacherFunction): string {
+//   return `${firstName}. ${lastName}`;
+// }
+
+// console.log(printTeacher({ firstName: "John", lastName: "Doe" }));
 
 
 
