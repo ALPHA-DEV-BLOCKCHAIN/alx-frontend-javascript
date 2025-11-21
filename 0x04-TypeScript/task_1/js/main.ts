07989e0125a0d24c1343cd200277f7454cf9cd16
ALPHA-DEV-BLOCKCHAIN <ibrahimmainainc@gmail.com>
2025-11-21 10:17:28 +0300

    printing  Teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,12 +11,47 @@ interface Director extends Teacher {
   numberOfReports: number;
 }
 
-const director1: Director = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
+// 3. Interface describing the function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// 3. Function implementation
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName} ${lastName}`;
 };
 
-console.log(director1);
+// Test output
+console.log(printTeacher("John", "Doe"));
+
+
+
+
+
+
+
+
+
+
+// interface Teacher {
+//   readonly firstName: string;
+//   readonly lastName: string;
+//   fullTimeEmployee: boolean;
+//   yearsOfExperience?: number;
+//   location: string;
+//   [key: string]: any;
+// }
+
+// interface Director extends Teacher {
+//   numberOfReports: number;
+// }
+
+// const director1: Director = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   location: 'London',
+//   fullTimeEmployee: true,
+//   numberOfReports: 17,
+// };
+
+// console.log(director1);
