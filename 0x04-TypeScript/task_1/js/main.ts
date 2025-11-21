7e2324b35e2166f01d64bf3848c6adcd85a4e50f
ALPHA-DEV-BLOCKCHAIN <ibrahimmainainc@gmail.com>
2025-11-21 10:28:32 +0300

    class defination error sorted in  writing a class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,15 +1,13 @@
-// Interface for the constructor
 interface StudentConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
 }
 
-// Interface for the class
 interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
 }
 
-class StudentClass implements StudentClassInterface {
+class StudentClass {
   private firstName: string;
   private lastName: string;
 
@@ -28,6 +26,44 @@ class StudentClass implements StudentClassInterface {
 }
 
 export { StudentClass };
+
+
+
+
+
+
+
+
+// // Interface for the constructor
+// interface StudentConstructor {
+//   new (firstName: string, lastName: string): StudentClassInterface;
+// }
+
+// // Interface for the class
+// interface StudentClassInterface {
+//   workOnHomework(): string;
+//   displayName(): string;
+// }
+
+// class StudentClass implements StudentClassInterface {
+//   private firstName: string;
+//   private lastName: string;
+
+//   constructor(firstName: string, lastName: string) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
+
+//   workOnHomework(): string {
+//     return "Currently working";
+//   }
+
+//   displayName(): string {
+//     return this.firstName;
+//   }
+// }
+
+// export { StudentClass };
 
 
 
